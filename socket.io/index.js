b49ac495c70488d9a28d86a2e335dce9d64913fb
sani <ysani095@gmail.com>
2024-02-17 18:39:01 +0800

    updated

--- socket.io/index.js
+++ socket.io/index.js
@@ -1,24 +1,37 @@
 import express from "express";
 import http from "http";
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
 import { Server } from "socket.io";
-
-const App = express();
-const server = http.createServer(App);
-const io = new Server(server);
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 const PORT = 2000;
+const App = express();
+// const httpServer = http.createServer(App);
+const expressServer = App.listen(PORT, () => {
+  console.log(`Server listening at port: http://localhost:${PORT}`);
+});
+// const io = new Server(httpServer);
+const io = new Server(expressServer);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
-console.log(join(__dirname, "public", "index.html"));
+App.use(express.static("public"));
 
-App.get("/", (req, res) =>
-  res.sendFile(join(__dirname, "public", "index.html"))
-);
+io.on("connection", (client) => {
+  console.log("io user is connected");
+  client.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${client.id}: ${message}`);
+  });
 
-io.on("connection", () => {
-  console.log("io server connected");
+  client.on("disconnect", () => {
+    io.emit("out", " user disconnected");
+  });
 });
 
-server.listen(PORT, () => console.log("server listening on port 200"));
+// App.get("/", (req, res) => {
+//   res.sendFile(join(__dirname, "public", "index.html"));
+// });
+
+// httpServer.listen(PORT, () =>
+//   console.log(`Server listening at port: http://localhost:${PORT}`)
+// );
